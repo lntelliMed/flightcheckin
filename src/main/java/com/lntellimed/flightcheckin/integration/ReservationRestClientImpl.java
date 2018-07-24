5bf3d811ae26809f7d756b53b1cbe0c3218b9bdc
“Sam <sam.alsmadi@gmail.com>
2018-07-24 18:32:23 -0400

    Implement Reservation REST client

--- src/main/java/com/lntellimed/flightcheckin/integration/ReservationRestClientImpl.java
+++ src/main/java/com/lntellimed/flightcheckin/integration/ReservationRestClientImpl.java
@@ -1,20 +1,25 @@
 package com.lntellimed.flightcheckin.integration;
 
+import org.springframework.web.client.RestTemplate;
+
 import com.lntellimed.flightcheckin.integration.dto.Reservation;
 import com.lntellimed.flightcheckin.integration.dto.ReservationUpdateRequest;
 
 public class ReservationRestClientImpl implements ReservationRestClient {
+	private String RESERVATION_REST_URL = "http://localhost:8080/flightreservation/reservations/";
 
 	@Override
 	public Reservation findReservation(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		RestTemplate restTemplate = new RestTemplate();
+		Reservation reservation = restTemplate.getForObject(RESERVATION_REST_URL + id, Reservation.class);
+		return reservation;
 	}
 
 	@Override
 	public Reservation updateReservation(ReservationUpdateRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		RestTemplate restTemplate = new RestTemplate();
+		Reservation reservation = restTemplate.postForObject(RESERVATION_REST_URL, request, Reservation.class);
+		return reservation;
 	}
 
 }
